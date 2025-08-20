c17a10d4e8f92c00e8003a7dc049f65d8568bf5d
Caio Seabra de Queiroz <144072410+caioxdev@users.noreply.github.com>
2025-08-20 18:48:43 -0300

    Delete Teste - java/Teste - java directory

--- Teste - java/Teste - java/src/EmojiTeste.java
+++
@@ -1,6 +0,0 @@
-public class EmojiTeste {
-    public static void main(String[] args) {
-        System.out.println("✔ Pedido feito");
-        System.out.println("Testando emojis \uD83D\uDE00");
-    }
-}
