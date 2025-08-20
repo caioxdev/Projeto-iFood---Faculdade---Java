2a0d298dc5636a49b1f8bbae4c00497132be9178
Caio Seabra de Queiroz <144072410+caioxdev@users.noreply.github.com>
2025-08-20 18:47:27 -0300

    Add files via upload

---
+++ Teste - java/Teste - java/src/EmojiTeste.java
@@ -0,0 +1,6 @@
+public class EmojiTeste {
+    public static void main(String[] args) {
+        System.out.println("✔ Pedido feito");
+        System.out.println("Testando emojis \uD83D\uDE00");
+    }
+}
